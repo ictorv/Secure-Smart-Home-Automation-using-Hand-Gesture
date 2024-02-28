364870c14dbf7d84208cfb1661a58ef51484f11c
ictorv <victor.mygit@gmail.com>
2024-02-29 03:37:55 +0530

    Structured Files

--- README.md
+++ README.md
@@ -2,7 +2,64 @@
 ```bash
 An IoT project for home Automation
 ```
+# Description of the Problem:
+Controlling home devices through hand gestures presents an  innovative and intuitive approach to home automation. This proposal aims to leverage advancements in computer vision and machine learning to develop a robust system enabling users to controlvarious home appliances through hand gestures.
 
 ## Project Flowchart
 ![Flowchart](https://github.com/ictorv/Secure-Smart-Home-Automation-using-Hand-Gesture/blob/main/resources/flowchart.png)
 
+## Built With
+[![Python](https://img.shields.io/badge/python-3776AB?style=for-the-badge&logo=python&logoColor=ffdd54)](https://python.org/)
+![TensorFlow](https://img.shields.io/badge/TensorFlow-%23FF6F00.svg?style=for-the-badge&logo=TensorFlow&logoColor=white) ![Keras](https://img.shields.io/badge/Keras-%23D00000.svg?style=for-the-badge&logo=Keras&logoColor=white) 
+[![OpenCV](https://img.shields.io/badge/opencv-%230065F4.svg?style=for-the-badge&logo=opencv&logoColor=white)](https://opencv.org/)
+[![Resend](https://img.shields.io/badge/resend-%23000000.svg?style=for-the-badge&logo=resend&logoColor=white)](https://resend.com/)
+[![Raspberry Pi](https://img.shields.io/badge/raspberrypi-%23ff69b4.svg?style=for-the-badge&logo=raspberrypi&logoColor=white)](https://www.raspberrypi.org/)
+
+## Hardware
+| Component                    |
+| ---------------------------- |
+| Raspberry Pi 4 Model B       |
+| REES52 Raspberry Pi Camera   |
+| REES52 8 Channel DC 5V Relay |
+| LED bulb(s) and holders      |
+| AC Fan Hardware              |
+| Jumper wires                 |
+| Resistor                     |
+| Breadboard                   |
+| SD Card                      |
+| Micro HDMI to HDMI cable     |
+
+
+## Built By
+<table>
+<tr>
+    <td align="center">
+        <a href="https://github.com/ictorv">
+            <img src="https://avatars.githubusercontent.com/u/106619187?v=4" width="100;" alt="ictorv"/>
+            <br />
+            <sub><b>Victor</b></sub>
+        </a>
+    </td>
+    <td align="center">
+        <a href="https://github.com/ighoshsubho">
+            <img src="https://avatars.githubusercontent.com/u/93722719?v=4" width="100;" alt="bartick"/>
+            <br />
+            <sub><b>Subho Ghosh</b></sub>
+        </a>
+    </td>
+    <td align="center">
+        <a href="https://github.com/Aditya007Datta">
+            <img src="https://avatars.githubusercontent.com/u/121566854?v=4" width="100;" alt="Aditya007Datta"/>
+            <br />
+            <sub><b>Aditya Dutta</b></sub>
+        </a>
+    </td>
+    <td align="center">
+        <a href="https://github.com/Jyotiradityaroy">
+            <img src="https://avatars.githubusercontent.com/u/109747774?v=4" width="100;" alt="Jyotiradityaroy"/>
+            <br />
+            <sub><b>Jyotiraditya Roy</b></sub>
+        </a>
+    </td>
+    </tr>
+</table>
---
+++ main.py
@@ -0,0 +1,127 @@
+import cv2
+import face_recognition
+import resend
+import random
+from dotenv import load_dotenv
+import os
+
+import numpy as np
+import subprocess
+from tensorflow.keras.models import load_model
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(3,GPIO.OUT)
+model = load_model('./models/model.h5')
+subprocess.run(["libcamera-vid", "--codec", "libav", "-o", "test.mp4", "-t", "8000", "--width", "640", "--height", "480"])
+
+'''Preprocess Input image to create it as trained data'''
+def preprocess_image(frame):
+    resized_frame = cv2.resize(frame, (150, 150))
+    grayscale_frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2GRAY)
+    processed_frame = np.expand_dims(grayscale_frame, axis=-1)
+    processed_frame = processed_frame / 255.0
+    return processed_frame
+
+'''Recognition using Model'''
+def recognize_gesture(frame):
+    input_data = preprocess_image(frame)
+    input_data = np.expand_dims(input_data, axis=0)
+    prediction = model.predict(input_data)
+    predicted_class = np.argmax(prediction)
+    return predicted_class
+
+'''Security Key'''
+load_dotenv()
+resend.api_key = "API_KEY_HERE" # Add API KEY 
+
+'''Mail Sending Function'''
+def resend_api(to_person: str, length=4):
+    try:
+        min_value = 10**(length-1)
+        max_value = (10**length) - 1
+        MFA_Code = random.randint(min_value, max_value)
+
+        params = {
+            "from": "onboarding@resend.dev",
+            "to": ["victor.myid@gmail.com"],
+            "subject": "MFA Code",
+            "html": f"<strong>Sign in code: {MFA_Code}</strong>"
+        }
+
+        r = resend.Emails.send(params)
+        print(r)
+
+        return MFA_Code
+    except:
+        return -999
+
+'''Image input and Encoding'''
+known_image = face_recognition.load_image_file("./known_database/obama.jpg")
+known_encoding = face_recognition.face_encodings(known_image)[0]
+
+'''Capturing Images'''
+cap=cv2.VideoCapture('test.mp4')
+
+'''Flag variable'''
+loop=1
+verified=0
+
+'''Recognition and Mail'''
+while True:
+    ret, frame = cap.read()
+
+    # Find all face locations in the frame
+    face_locations = face_recognition.face_locations(frame)
+    face_encodings = face_recognition.face_encodings(frame, face_locations)
+
+    # Compare each face encoding with the known face encoding
+    for face_encoding in face_encodings:
+        # Compare the current face encoding with the known face encoding
+        match = face_recognition.compare_faces([known_encoding], face_encoding)
+
+        # If a match is found, label the face
+        if match[0]:
+            loop=0 # For ending loop
+            verified=1 # To send flag that verified person want to access
+            # cv2.putText(frame, "Known Face", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cap.release()
+            cv2.destroyAllWindows()
+            if __name__ == "__main__":
+                code=resend_api("victor.myid@gmail.com")
+                check=int(input("Enter Verification Code:"))
+                if (code == check) and (code!=-999):
+                    print("Verification Completed!")
+            
+    # Display the frame
+    if loop:
+        cv2.imshow('Video', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        cap.release()
+        cv2.destroyAllWindows()
+        break
+
+'''Recogniton and GPIO Control'''
+if verified:
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Failed to capture frame")
+            break
+        cv2.imshow('Camera', frame)
+        gesture_class = recognize_gesture(frame)
+        if gesture_class==1:
+            GPIO.output(3,True)
+        else:
+            GPIO.output(3,False)
+            #GPIO.output(11,False)
+        #if gesture_class==3:
+            #GPIO.output(11,False)
+        print("Recognized Class:", gesture_class)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
