98f626424907d3650ec975712ebd44c30bb3ebc2
ighoshsubho <ighoshsubho@gmail.com>
2024-02-28 20:50:38 +0530

    Resend api integrated

---
+++ email_api/resend.py
@@ -0,0 +1,28 @@
+import os
+import resend
+import random
+from dotenv import load_dotenv
+
+load_dotenv()
+
+resend.api_key = os.environ["RESEND_API_KEY"]
+
+def resend_api(to_person: str, length=4):
+    try:
+        min_value = 10**(length-1)
+        max_value = (10**length) - 1
+        MFA_Code = random.randint(min_value, max_value)
+        params = {
+            "from": "onboarding@resend.dev",
+            "to": [to_person],
+            "subject": "MFA Code",
+            "html": f"<strong>Sign in code: {MFA_Code}</strong>"
+        }
+
+        r = resend.Emails.send(params)
+        print(r)
+
+        return ("Ok", MFA_Code)
+    except:
+        return ("Error", -999)
+    pass
